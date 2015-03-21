c746b33f85699265ce9fc3c1a5df7ce27efae079
Paolo Scanferla <paolo.scanferla@mondora.com>
2015-03-21 16:01:29 +0100

    Changed routes and assets paths to support html5 history

--- app/lib/routes.jsx
+++ app/lib/routes.jsx
@@ -6,7 +6,7 @@ var views = require("views");
 module.exports = (
     <Router.Route name="root" path="/" handler={views.Root}>
         <Router.Route name="kindle" path="m/:name/kindle" handler={views.Kindle} />
-        <Router.Route name="m" path="m/:name" handler={views.M} />
+        <Router.Route name="m" path="m/:name/" handler={views.M} />
         <Router.Route name="profile" path="profile" handler={views.Profile} />
         <Router.Route name="s" path="s/:id" handler={views.S} />
         <Router.DefaultRoute handler={views.NotFound} />
